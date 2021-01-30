cb4027e448195b6268b414b05ce298553e7884ea
serein <619030878@qq.com>
2021-01-31 02:32:07 +0800

    更改路由模式

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
     routes,
-    mode: "history"
+    mode: process.env.VUE_APP_ENV === "prod" ? "hash" : "history",
 });
 
 router.beforeEach(async (to, from, next) => {
